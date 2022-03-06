468520c1c42814b2234c2294b96b8f4309800129
Steve Alomia <stevealomia@yahoo.com>
2022-03-06 17:03:26 -0500

    commit tree ba05db8c83aa723a9551c69f719de4c2977f019f

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+    console.log(cats);
+};
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+    console.log(cats);
+};
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+    console.log(cats);
+};
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+    console.log(cats);
+};
+
+//comeback to this one
+function appendCat(name) {
+    const newCats = [...cats, name];
+    return  newCats;
+};
+// comeback here 
+function prependCat(name) {
+    const newCats = [name, ...cats];
+    return  newCats;
+};
+
+function removeLastCat() {
+    return cats.slice(0,-1);
+    
+};
+
+function removeFirstCat() {
+    console.log(cats.slice(1));
+    return cats.slice(1); 
+};
